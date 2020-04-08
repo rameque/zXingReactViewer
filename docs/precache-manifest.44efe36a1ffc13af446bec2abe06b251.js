27acd4b3c58600bafee8330d85bc0df0f8164895
Ramiro Gomez <rfgo40@gmail.com>
2020-04-08 08:57:21 -0500

    feat: docs for test with https

---
+++ docs/precache-manifest.44efe36a1ffc13af446bec2abe06b251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e599258e7b8e5e73691e83031329d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cbe80cecd49add401bb",
+    "url": "/static/css/main.6d8e181c.chunk.css"
+  },
+  {
+    "revision": "16fa27ed3965cf5c02b9",
+    "url": "/static/js/2.57fb7621.chunk.js"
+  },
+  {
+    "revision": "9545d2ff040f1d230f20c359158fcfad",
+    "url": "/static/js/2.57fb7621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbe80cecd49add401bb",
+    "url": "/static/js/main.86b75ed6.chunk.js"
+  },
+  {
+    "revision": "50340ae7c68906abfcb8",
+    "url": "/static/js/runtime-main.f6a3923d.js"
+  }
+]);
